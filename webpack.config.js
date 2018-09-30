7519a4378aaef5554510628fbd898d9e24a9bdbd
ahuang <mengling915@163.com>
2018-09-30 14:24:36 +0800

    added: webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin');
+
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +10,7 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  mode: 'development',
   module: {
     rules: [
       {
@@ -59,26 +62,19 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins: [
+    new CompressionPlugin() 
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
+  module.exports.mode = 'production';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
-    })
+    }),    
   ])
 }
